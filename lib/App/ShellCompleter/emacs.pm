c532c75ce86bc967fc9677e56f50cee9453de13c
Steven Haryanto (on PC, Bandung) <stevenharyanto@gmail.com>
2015-02-15 20:45:59 +0700

    Initial commit

---
+++ lib/App/ShellCompleter/emacs.pm
@@ -0,0 +1,15 @@
+package App::ShellCompleter::emacs;
+
+# DATE
+# VERSION
+
+1;
+# ABSTRACT: Shell completion for emacs
+
+=head1 SYNOPSIS
+
+See L<_emacs> included in this distribution.
+
+
+=head1 SEE ALSO
+
